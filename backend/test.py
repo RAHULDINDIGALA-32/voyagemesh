e0cea04d72d97d94530ee8c623107147202ae996
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2026-09-18 01:20:51 +0530

    feat(backend): add flight tool for flights-search

--- backend/test.py
+++ backend/test.py
@@ -1,4 +1,8 @@
 from tools.tavily import tavily_search
+from tools.flight import search_flights
 
-tavily_test_response = tavily_search("What are the best hotels in South Munbai, India")
-print(tavily_test_response)
+tavily_tool_test_response = tavily_search("What are the best hotels in South Munbai, India")
+print(tavily_tool_test_response)
+
+flight_tool_test_response = search_flights("Plan a 7 days trip to Japan from Hyderabad, India")
+print(flight_tool_test_response)
