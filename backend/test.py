89bc7e23e85a676d01c780673b8641464e156d63
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2026-09-19 14:19:10 +0530

    feat(backend): add agents pipeline with langggraph

--- backend/test.py
+++ backend/test.py
@@ -1,8 +1,12 @@
 from tools.tavily import tavily_search
 from tools.flight import search_flights
+from agent import run_travel_agent
 
-tavily_tool_test_response = tavily_search("What are the best hotels in South Munbai, India")
-print(tavily_tool_test_response)
+#tavily_tool_test_response = tavily_search("What are the best hotels in South Munbai, India")
+#print(tavily_tool_test_response)
 
-flight_tool_test_response = search_flights("Plan a 7 days trip to Japan from Hyderabad, India")
-print(flight_tool_test_response)
+#flight_tool_test_response = search_flights("Plan a 7 days trip to Japan from Hyderabad, India")
+#print(flight_tool_test_response)
+
+agent_pipeline_test_response = run_travel_agent("Plan a 7 days trip to Japan from Hyderabad, India")
+print(agent_pipeline_test_response)
